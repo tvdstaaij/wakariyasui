9d0900fe9f46d574d2c065b20da4e21006fcd19c
Tim van der Staaij <git@tim.vanderstaaij.email>
2016-10-01 16:42:07 +0200

    Use lazy init, add page annotation feature

--- wakariyasui.js
+++ wakariyasui.js
@@ -21,70 +21,100 @@
 // @resource unk_pos.dat.gz https://github.com/tvdstaaij/kuromoji.js/raw/greasemonkey/dict-base91/unk_pos.dat.gz
 // ==/UserScript==
 
-// True: Script functionality is always loaded (performance hit!)
-// False: Ctrl-Alt-W loads the script functionality
-var IMMEDIATE_INIT = false;
-
 (function(){
-  var initialized = false;
-  function triggerInit(e) {
-    if (
-      initialized ||
-      e.keyCode !== getCharCode('W') ||
-      e.metaKey || e.shiftKey ||
-      !e.altKey || !e.ctrlKey
-    ) return;
-    initialized = true;
-    init();
-  }
-  if (IMMEDIATE_INIT) init();
-  else document.addEventListener('keydown', triggerInit, false);
+    var initState = 'idle';
+    document.addEventListener('keydown', function(e) {
+        var action = decideAction(e);
+        if (!action) return;
+        switch (initState) {
+            case 'ready':
+                action();
+                break;
+            case 'idle':
+                initState = 'initializing';
+                console.log('Loading kuroshiro');
+                kuroshiro.init({dicPath: ''}, function(err) {
+                    if (err) {
+                        console.error('Failed to load kuroshiro:', err);
+                        initState = 'idle';
+                        return;
+                    }
+                    initState = 'ready';
+                    console.log('Kuroshiro ready');
+                    action();
+                });
+                break;
+        }
+    }, false);
 })();
 
-function init() {
-  console.log('Loading kuroshiro');
-  kuroshiro.init({dicPath: ''}, function(err) {
-    if (err) {
-      console.error('Failed to load kuroshiro:', err);
-      return;
+function decideAction(e) {
+    if (e.metaKey || !e.ctrlKey || !e.altKey) return null;
+    var action = null;
+
+    var kanaType = e.shiftKey ? 'katakana' : 'hiragana';
+    switch (e.keyCode) {
+        case getCharCode('W'):
+            action = function(){}; // Just init
+            break;
+        case getCharCode('A'):
+            action = annotateTextNodes;
+            break;
+        case getCharCode('O'):
+            action = annotateInput.bind(this, {to: kanaType, mode: 'okurigana'});
+            break;
+        case getCharCode('K'):
+            action = annotateInput.bind(this, {to: kanaType, mode: 'normal'});
+            break;
+        case getCharCode('S'):
+            action = annotateInput.bind(this, {to: kanaType, mode: 'spaced'});
+            break;
+        case getCharCode('R'):
+            action = annotateInput.bind(this, {to: 'romaji', mode: e.shiftKey ? 'okurigana' : 'normal'});
+            break;
     }
-    console.log('Kuroshiro ready, registering hotkeys');
-    document.addEventListener('keydown', convertActiveElement, false);
-  });
+    return action;
 }
 
-function convertActiveElement(e) {
-  if (e.metaKey || !e.ctrlKey || !e.altKey) return;
-  var options = null;
-  var kanaType = e.shiftKey ? 'katakana' : 'hiragana';
-  switch (e.keyCode) {
-    case getCharCode('O'):
-      options = {to: kanaType, mode: 'okurigana'};
-      break;
-    case getCharCode('K'):
-      options = {to: kanaType, mode: 'normal'};
-      break;
-    case getCharCode('S'):
-      options = {to: kanaType, mode: 'spaced'};
-      break;
-    case getCharCode('R'):
-      options = {to: 'romaji', mode: e.shiftKey ? 'okurigana' : 'normal'};
-      break;
-  }
-  if (!options) return;
-  var curElement = document.activeElement;
-  switch (curElement.tagName.toLowerCase()) {
-    case 'textarea':
-    case 'input':
-      curElement.value = kuroshiro.convert(curElement.value, options);
-      break;
-    default:
-      if (!curElement.getAttribute('contenteditable')) break;
-      curElement.innerHTML = kuroshiro.convert(curElement.textContent, options);
-  }
+function annotateInput(options) {
+    var curElement = document.activeElement;
+    switch (curElement.tagName.toLowerCase()) {
+        case 'textarea':
+        case 'input':
+            curElement.value = kuroshiro.convert(curElement.value, options);
+            break;
+        default:
+            if (!curElement.getAttribute('contenteditable')) break;
+            curElement.innerHTML = kuroshiro.convert(curElement.textContent, options);
+    }
+}
+
+function annotateTextNodes() {
+    var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+    var node, kanjiNodes = [];
+    while (node = walker.nextNode()) {
+        tagName = node.parentElement? node.parentElement.tagName : '';
+        if (tagName === 'SCRIPT' || tagName === 'STYLE' || tagName === 'RUBY' ||
+            tagName === 'RB' || tagName === 'RT') continue;
+        if (!kuroshiro.hasKanji(node.nodeValue)) continue;
+        kanjiNodes.push(node);
+    }
+    console.log('Annotating ' + String(kanjiNodes.length) + ' elements containing kanji');
+    kanjiNodes.forEach(function(node) {
+       var text = node.nodeValue;
+       var annotatedText;
+       try {
+          annotatedText = kuroshiro.convert(text, {to: 'hiragana', mode: 'furigana'});
+       } catch (err) {
+           console.error('Kuroshiro:', err);
+       }
+       if (annotatedText === undefined) return;
+       var annotatedNode = document.createElement('span');
+       annotatedNode.innerHTML = annotatedText;
+       node.parentNode.replaceChild(annotatedNode, node);
+    });
 }
 
 function getCharCode(str) {
-  return str.charCodeAt(0);
+    return str.charCodeAt(0);
 }
-
